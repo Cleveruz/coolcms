223f375834acbf684c7bb127998e44a5bb0faa5e
midreg <support@midreg.com>
2020-03-02 09:35:36 +0200

    footer, header, bootstrap

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'test';
+require_once 'header.php';
+?>
+
+
+<div class="card mt-1 border-light">
+    <div class="card-body">
+        test
+    </div>
+</div>
+
+<?php
+require_once 'footer.php';
