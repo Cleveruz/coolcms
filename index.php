12f3ab8c1faf2210115bfaae163a97dbeb7369f3
midreg <support@midreg.com>
2020-03-04 00:01:16 +0200

    latest chat messages

--- index.php
+++ index.php
@@ -5,8 +5,30 @@ require_once 'header.php';
 
 
 <div class="card mt-1 border-light">
+    <div class="card-header">
+        <ul class="nav nav-tabs card-header-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" href="chat.php"><?=$lang['chat']?></a>
+            </li>
+        </ul>
+    </div>
+
     <div class="card-body">
-        test
+        <?php
+            $chat_result = mysqli_query($conn, "select * from chat order by time desc limit 5");
+        ?>
+
+        <h5><?=$lang['latest_messages_from_chat']?></h5>
+
+        <?php foreach($chat_result as $post):?>
+            <?=username($post['user_id'])?>: <?=$post['message']?><br>
+        <?php endforeach;?>
+    </div>
+</div>
+
+<div class="card mt-1 border-light">
+    <div class="card-body">
+        <?=$lang['download_coolcms_from']?>
     </div>
 </div>
 
