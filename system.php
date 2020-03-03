33b6e0a1504e189389674d163fcc644076c3b980
midreg <support@midreg.com>
2020-03-03 09:07:57 +0200

    adding extra language

--- system.php
+++ system.php
@@ -2,7 +2,7 @@
 
 $config = parse_ini_file('config.ini');
 
-$lang = parse_ini_file('language/en.ini');
+$lang = parse_ini_file('language/' . $config['lang'] . '.ini');
 
 $conn = mysqli_connect('localhost', $config['db_user'], $config['db_password'], $config['db_name']);
 
