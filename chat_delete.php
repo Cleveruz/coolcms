6ebeb97a3a3ec44a975f0259b70fb2496c9cebee
midreg <support@midreg.com>
2020-03-04 14:50:47 +0200

    [fix] delete someone's message

--- chat_delete.php
+++ chat_delete.php
@@ -1,11 +1,15 @@
 <?php
 require_once 'system.php';
 
+auth_only();
+
 $id = $_GET['id'];
 
-mysqli_query($conn, "delete from chat where id = $id");
+$result = mysqli_query($conn, "select * from chat where id = $id");
+$post = mysqli_fetch_assoc($result);
 
-header('location: chat.php');
-exit;
+if ($post['user_id'] == $auth['id']) {
+    mysqli_query($conn, "delete from chat where id = $id");
+}
 
-?>
+redirect('chat.php');
